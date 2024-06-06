3cd1c441b9242a83f801c5a0cc0e0ff294f808ca
kotelnikov-vladislav <vladislav.kotelnikov@internet.ru>
2024-06-06 21:29:46 +0300

    main: корсы разрешены для всех запросов для клиента

--- client/app/axiosClient.ts
+++ client/app/axiosClient.ts
@@ -3,6 +3,11 @@ import axios from 'axios';
 const instance = axios.create({
 	withCredentials: true,
 	baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}`,
+	headers: {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+		'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+	},
 });
 
 export default instance;
